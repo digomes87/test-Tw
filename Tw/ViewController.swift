240ece8c7f1d7bf680852e873fbec8e8931e9faf
Nylon <diego.gomes87@gmail.com>
2015-11-09 14:52:19 -0200

    test of App same twitter

--- Tw/ViewController.swift
+++ Tw/ViewController.swift
@@ -7,8 +7,35 @@
 //
 
 import UIKit
+import Social
+
 
 class ViewController: UIViewController {
+    @IBAction func tweetButton(sender: UIButton) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter)
+        {
+            var score = 9000
+            var follow  = "@nylondev"
+            
+            let tController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+                tController.setInitialText("My power is more than \(score) so follow me \(follow)")
+            
+            self.presentViewController(tController,animated:true,completion:nil)
+        } else {
+           let alert = UIAlertController(title: "Account", message: "Login on Twitter", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            alert.addAction(UIAlertAction(title: "Settings", style: UIAlertActionStyle.Default, handler: {(UIAlertAction)in
+                let settingURL = NSURL(string: UIApplicationOpenSettingsURLString)
+                if let url = settingURL{
+                    UIApplication.sharedApplication().openURL(url)
+                }
+        
+            }))
+            
+            self.presentViewController(alert, animated: true, completion:nil)
+            
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
